23c0e41b0013b4f483245de2eb8e9fab27644c76
abhinaykourav11-gif <abhinaykourav11@gmail.com>
2026-08-12 16:38:35 +0530

    Create Problem1.java

---
+++ Assignment-3/Problem1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        long B = sc.nextLong();
+
+        int[] scholarship = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            scholarship[i] = sc.nextInt();
+        }
+
+        Arrays.sort(scholarship);
+
+        int count = 0;
+        long sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (sum + scholarship[i] <= B) {
+                sum += scholarship[i];
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
